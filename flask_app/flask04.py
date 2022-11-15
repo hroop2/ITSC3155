5a1426574bd91ef324233145341ce63ec0992dbb
Hannah Roop <hroop2@uncc.edu>
2022-11-15 00:18:12 -0500

    flask app #4 - db queries

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -7,12 +7,16 @@ from flask import render_template
 from flask import request
 from flask import redirect, url_for 
 from database import db
+from models import Note as Note
+from models import User as User
 
 app = Flask(__name__)     # create an app
-notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
-             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-2-2020'}
-             }
+
+
+#notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
+#            2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+#             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-2-2020'}
+#            }
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
@@ -29,44 +33,64 @@ with app.app_context():
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Hannah', "email": 'mogli@uncc.edu'}
-    return render_template('index.html', user=a_user)
+    a_user = db.session.query(User).filter_by(email="mogli@uncc.edu")
+    return render_template("index.html", user=a_user)
 
 
 
 
 @app.route('/notes')
 def get_notes():
-    a_user = {'name': 'Hannah', "email": 'mogli@uncc.edu'}
+    #a_user = {'name': 'Hannah', "email": 'mogli@uncc.edu'}
 
-    return render_template('notes.html', notes=notes, user=a_user)
+    a_user=db.session.query(User).filter_by(email='mogli@uncc.edu')
+    #retrieve user from database
+    my_notes=db.session.query(Note).all()
+    #retrieve notes from database
+
+    return render_template('notes.html', notes=my_notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = {'name': 'Hannah', "email": 'mogli@uncc.edu'}
+
+    #a_user = {'name': 'Hannah', "email": 'mogli@uncc.edu'}
     #app = Flask(__name__)
 
-    return render_template('note.html', note=notes[int(note_id)], user=a_user)  
+    a_user = db.session.query(User).filter_by(email='molgi@uncc.edu')
+    #retrieve user from database
+    my_note=db.session.query(Note).filter_by(id=note_id)
+
+    #return render_template('note.html', note=notes[int(note_id)], user=a_user)  
+    return render_template('note.html', note=my_note, user=a_user)
  
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
-    a_user={'name': 'Hannah','email':'hroop2@uncc.edu'}
-    
+    #a_user={'name': 'Hannah','email':'hroop2@uncc.edu'}
+    #check method used for request
     if request.method == 'POST':
+        #get title data
         title = request.form['title']
+        #get note data
         text = request.form['noteText']
+        #create date stamp
         from datetime import date
         today= date.today()
+        # format date mm/dd/yyyyy
         today = today.strftime("%m-%d-%Y")
-        id  = len(notes)+1
-        notes[id] = {'title': title, 'text': text, 'date': today}
+        #id  = len(notes)+1
+        #notes[id] = {'title': title, 'text': text, 'date': today}
+        #return redirect(url_for('get_notes'))
+        new_record= Note(title, text, today)
+        db.sessiom.add(new_record)
+        db.session.commit()
+
+
+        #equest_data = request.form
+        #return f"data: {request_data} !"
         return redirect(url_for('get_notes'))
-
-
-        request_data = request.form
-        return f"data: {request_data} !"
         
     else:
+        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
         return render_template('new.html', user = a_user)
 
     
